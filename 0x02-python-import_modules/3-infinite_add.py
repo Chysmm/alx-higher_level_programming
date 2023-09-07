9012cec34f84063a23574dd5e918af81110840c5
Chysmm <som4nick@yahoo.com>
2023-09-07 13:13:36 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 sum = 0
-
 if __name__ == "__main__":
-    for item in argv:
-        if argv.index(item) == 0:
-            continue
-        sum += int(item)
-
-    print(sum)
-
+for item in argv:
+if argv.index(item) == 0:
+continue
+sum += int(item)
+print(sum)
