5d4f6c5a8fb19a269c0304e3af5be6a522b4b157
Chysmm <som4nick@yahoo.com>
2023-09-07 13:05:32 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
- """Handle basic arithmetic operations."""
+from sys import argv
 from calculator_1 import add, sub, mul, div
-import sys
-if len(sys.argv) - 1 != 3:
-print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-sys.exit(1)
-ops = {"+": add, "-": sub, "*": mul, "/": div}
-if sys.argv[2] not in list(ops.keys()):
-print("Unknown operator. Available operators: +, -, * and /")
-sys.exit(1)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b))
+
+if __name__ == "__main__":
+    argv_len = len(argv)
+    if argv_len != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operands = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = argv[2]
+    if operator not in operands:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, operands[operator](a, b)))
